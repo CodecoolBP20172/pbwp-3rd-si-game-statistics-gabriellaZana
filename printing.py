54997db1ba4636eca7ff81c40798d8593981e9cf
gabriellaZana <zanagabriella@gmail.com>
2017-06-26 21:04:53 +0200

    first five funcions' printing

--- printing.py
+++ printing.py
@@ -1,2 +1,33 @@
+import reports
+import pprint
 
-# Printing functions
+
+def print_count_games(file_name):
+    pprint.pprint(reports.count_games(file_name))
+
+
+def print_decide(file_name, year):
+    pprint.pprint(reports.decide(file_name, year))
+
+
+def print_get_latest(file_name):
+    pprint.pprint(reports.get_latest(file_name))
+
+
+def print_count_by_genre(file_name, genre):
+    pprint.pprint(reports.count_by_genre(file_name, genre))
+
+
+def print_get_line_number_by_title(file_name, title):
+    pprint.pprint(reports.get_line_number_by_title(file_name, title))
+
+
+def main():
+    print_count_games("game_stat.txt")
+    print_decide("game_stat.txt", "2004")
+    print_get_latest("game_stat.txt")
+    print_count_by_genre("game_stat.txt", "Simulation")
+    print_get_line_number_by_title("game_stat.txt", "The Sims")
+
+
+main()
