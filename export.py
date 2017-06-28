dbd17cdcc81fff28db8ec65205f14efa8b59612d
gabriellaZana <zanagabriella@gmail.com>
2017-06-28 21:39:16 +0200

    export with bonus functions

--- export.py
+++ export.py
@@ -3,7 +3,8 @@ import reports
 
 def export_the_reports(export_file_name, file_name, year, genre, title):
     results = [reports.count_games(file_name), reports.decide(file_name, year), reports.get_latest(file_name),
-               reports.count_by_genre(file_name, genre), reports.get_line_number_by_title(file_name, title)]
+               reports.count_by_genre(file_name, genre), reports.get_line_number_by_title(file_name, title),
+               reports.sort_abc(file_name), reports.get_genres(file_name), reports.when_was_top_sold_fps(file_name)]
     with open(export_file_name, "w") as export_file:
         for item in results:
             export_file.write("%s\n" % item)
