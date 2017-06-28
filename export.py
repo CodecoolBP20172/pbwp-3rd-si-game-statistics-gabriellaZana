ae79f832be2d33801caf6c60e4922e2448abf021
gabriellaZana <zanagabriella@gmail.com>
2017-06-28 20:06:45 +0200

    export function done

--- export.py
+++ export.py
@@ -1,2 +1,12 @@
+import reports
 
-# Export functions
+
+def export_the_reports(export_file_name, file_name, year, genre, title):
+    results = [reports.count_games(file_name), reports.decide(file_name, year), reports.get_latest(file_name),
+               reports.count_by_genre(file_name, genre), reports.get_line_number_by_title(file_name, title)]
+    with open(export_file_name, "w") as export_file:
+        for item in results:
+            export_file.write("%s\n" % item)
+
+
+export_the_reports("results.txt", "game_stat.txt", "2004", "Simulation", "The Sims")
