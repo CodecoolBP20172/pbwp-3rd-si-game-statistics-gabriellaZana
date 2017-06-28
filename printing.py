5bdfe06e7aeaccd4806371f855e352763f4772d5
gabriellaZana <zanagabriella@gmail.com>
2017-06-28 21:33:52 +0200

    printing with bonus functions

--- printing.py
+++ printing.py
@@ -22,12 +22,26 @@ def print_get_line_number_by_title(file_name, title):
     pprint.pprint(reports.get_line_number_by_title(file_name, title))
 
 
+def print_sort_abc(file_name):
+    pprint.pprint(reports.sort_abc(file_name))
+
+
+def print_get_genres(file_name):
+    pprint.pprint(reports.get_genres(file_name))
+
+
+def print_when_was_top_sold_fps(file_name):
+    pprint.pprint(reports.when_was_top_sold_fps(file_name))
+
+
 def main():
     print_count_games("game_stat.txt")
     print_decide("game_stat.txt", "2004")
     print_get_latest("game_stat.txt")
     print_count_by_genre("game_stat.txt", "Simulation")
     print_get_line_number_by_title("game_stat.txt", "The Sims")
-
+    print_sort_abc("game_stat.txt")
+    print_get_genres("game_stat.txt")
+    print_when_was_top_sold_fps("game_stat.txt")
 
 main()
