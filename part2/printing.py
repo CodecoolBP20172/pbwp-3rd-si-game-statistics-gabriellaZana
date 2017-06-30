f6de181466ad58727d9b82b1ac47fd79496002fc
gabriellaZana <zanagabriella@gmail.com>
2017-06-30 11:29:14 +0200

    working with the first 6 funcs

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,34 @@
+import reports
+import pprint
 
-# Printing functions
+
+def print_get_most_played(file_name):
+    pprint.pprint(reports.get_most_played(file_name))
+
+
+def print_sum_sold(file_name):
+    pprint.pprint(reports.sum_sold(file_name))
+
+
+def print_get_selling_avg(file_name):
+    pprint.pprint(reports.get_selling_avg(file_name))
+
+
+def print_count_longest_title(file_name):
+    pprint.pprint(reports.count_longest_title(file_name))
+
+
+def print_get_date_avg(file_name):
+    pprint.pprint(reports.get_date_avg(file_name))
+
+
+def print_get_game(file_name, title):
+    pprint.pprint(reports.get_game(file_name, title))
+
+
+print_get_most_played("game_stat.txt")
+print_sum_sold("game_stat.txt")
+print_get_selling_avg("game_stat.txt")
+print_count_longest_title("game_stat.txt")
+print_get_date_avg("game_stat.txt")
+print_get_game("game_stat.txt", "The Sims")
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,82 @@
+import math
 
-# Report functions
+
+def get_most_played(file_name):
+    games_datas_list = []
+    title_copies_sold_dict = dict()
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        title_copies_sold_dict[sublist[0]] = float(sublist[1])
+    result = max(title_copies_sold_dict, key=title_copies_sold_dict.get)
+    return result
+
+
+def sum_sold(file_name):
+    games_datas_list = []
+    sold_total = 0
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        sold_total += float(sublist[1])
+    return sold_total
+
+
+def get_selling_avg(file_name):
+    games_datas_list = []
+    sold_copies = []
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        sold_copies.append(float(sublist[1]))
+    return (sum(sold_copies)/len(sold_copies))
+
+
+def count_longest_title(file_name):
+    games_datas_list = []
+    titles = []
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        titles.append(sublist[0])
+    longest = max(titles, key=len)
+    return len(longest)
+
+
+def get_date_avg(file_name):
+    games_datas_list = []
+    years = []
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        years.append(int(sublist[2]))
+    return (math.ceil(sum(years)/len(years)))
+
+
+def get_game(file_name, title):
+    games_datas_list = []
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+        if title == sublist[0]:
+            sublist[1] = float(sublist[1])
+            sublist[2] = int(sublist[2])
+            return sublist
+
+
+'''def count_grouped_by_genre(file_name):
+    games_datas_list = []
+    with open(file_name) as input_file:
+        for line in input_file:
+            games_datas_list.append(line.strip().split('\t'))
+    for sublist in games_datas_list:
+
+    print(genre_count_dict)
+
+count_grouped_by_genre("game_stat.txt")'''
