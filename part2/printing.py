f271f9ffe48c6054ff4d17f9d23746271278e614
gabriellaZana <zanagabriella@gmail.com>
2017-07-01 19:27:47 +0200

    printing with bonus functions

--- part2/printing.py
+++ part2/printing.py
@@ -26,9 +26,23 @@ def print_get_game(file_name, title):
     pprint.pprint(reports.get_game(file_name, title))
 
 
-print_get_most_played("game_stat.txt")
-print_sum_sold("game_stat.txt")
-print_get_selling_avg("game_stat.txt")
-print_count_longest_title("game_stat.txt")
-print_get_date_avg("game_stat.txt")
-print_get_game("game_stat.txt", "The Sims")
+def print_count_grouped_by_genre(file_name):
+    pprint.pprint(reports.count_grouped_by_genre(file_name))
+
+
+def print_get_date_ordered(file_name):
+    pprint.pprint(reports.get_date_ordered(file_name))
+
+
+def main():
+    print_get_most_played("game_stat.txt")
+    print_sum_sold("game_stat.txt")
+    print_get_selling_avg("game_stat.txt")
+    print_count_longest_title("game_stat.txt")
+    print_get_date_avg("game_stat.txt")
+    print_get_game("game_stat.txt", "The Sims")
+    print_count_grouped_by_genre("game_stat.txt")
+    print_get_date_ordered("game_stat.txt")
+
+
+main()
